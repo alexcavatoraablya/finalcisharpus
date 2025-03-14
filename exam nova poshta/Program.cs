abdc6267786ba5853e9db34a7cbf5ebaccf11caa
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-03-14 14:30:42 +0200

    add class Service

--- exam nova poshta/Program.cs
+++ exam nova poshta/Program.cs
@@ -8,35 +8,56 @@
 
 
 using System.Diagnostics;
+using System.Numerics;
 using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-Console.WriteLine("--------- Welcome to Nova Poshta! ---------");
-Console.WriteLine("\tMail Services\n" +
-    "1. Add new service\n" +
-    "2. Save service\n" +
-    "3. Edit sertvice\n" +
-    "4. Show all service\n" +
-    "5. Find service\n" +
-    "6. Delete service\n");
-Console.WriteLine("-------------------------------------------");
+List<Package> items = new();
+List<Service> services = new();
 
-Console.Write("your choice: ");
-int choice = int.Parse(Console.ReadLine());
-
-Package item = null;
-
-switch (choice)
+while (true)
 {
-    case 1:
-        item.ReadFromConsole();
-        break;
+    Console.WriteLine("--------- Welcome to Nova Poshta! ---------");
+    Console.WriteLine("\tMail Services\n" +
+        "1. Add new package\n" +
+        "2. Add new service\n" +
+        "3. Show all package\n" +
+        "4. Show all service\n" +
+        "5. Find service\n" +
+        "6. Delete service\n");
+    Console.WriteLine("-------------------------------------------");
 
-    case 4:
-        item.Show();
-        break;
+    Console.Write("your choice: ");
+    int choice = int.Parse(Console.ReadLine());
+
+
+
+    switch (choice)
+    {
+        case 1:
+            Package newItem = new();
+            newItem.ReadFromConsole();
+            items.Add(newItem);
+            break;
+
+        case 4:
+            foreach (Package item in items)
+                item.Show();
+            break;
+        case 2:
+            Service newService = new();
+            newService.ReadFromConsole();
+            services.Add(newService);
+            break;
+
+        case 3:
+            foreach (Package item in items)
+                item.Show();
+            break;
+    }
+
+    Console.ReadKey();
 }
-
-Console.ReadKey();
 
 public class Package
 {
@@ -54,15 +75,42 @@ public class Package
         CityTo = Console.ReadLine();
         Console.Write("Enter city from: ");
         CityFrom = Console.ReadLine();
+        Console.Write("Enter phone: ");
         Phone = Console.ReadLine();
+        Console.Write("Enter date: ");
+        Date = Console.ReadLine();
     }
 
     public void Show()
     {
-        Console.WriteLine($"Number: {Number}$");
-        Console.WriteLine($"CityTo: {Date}");
-        Console.WriteLine($"Number: {Phone}");
-        Console.WriteLine($"CityFrom: {CityFrom}");
-        Console.WriteLine($"CityTo: {CityTo}");
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine($"Number: {Number}");
+        Console.WriteLine($"Date: {Date}");
+        Console.WriteLine($"Phone: {Phone}");
+        Console.WriteLine($"City from: {CityFrom}");
+        Console.WriteLine($"City to: {CityTo}");
+    }
+}
+public class Service
+{
+    public double Price { get; set; }
+    public int Duration { get; set; }
+    public string Name { get; set; }
+    
+    public void ReadFromConsole()
+    {
+        Console.Write("Enter name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter duration: ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.Write("Enter price: ");
+        Price = double.Parse(Console.ReadLine());
+    }
+    public void Show()
+    {
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Duration: {Duration}");
+        Console.WriteLine($"Price: {Price}");
     }
 }
