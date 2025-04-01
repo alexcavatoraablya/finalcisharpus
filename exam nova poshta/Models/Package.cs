4848f25e02e07617c6d0f67cda57a9e62256a576
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-04-01 14:34:51 +0300

    sorted to models

---
+++ exam nova poshta/Models/Package.cs
@@ -0,0 +1,41 @@
+﻿//Нова пошта
+//	Вивід послуг пошти
+//	Доповнення(редагування, видалення ) послуги
+//  Прийом замовлення про пересилку товару(друк квитанції, прийом товару, оплата, реєстрація про замовлення у базі) 
+//	Відмітка про виконання замовлення(доставку товарів)
+//	Зберігати та завантажувати у(з) файлі(у) замовлені послуги
+//	Виконані послуги зберігати у іншому файлі
+
+
+public class Package
+{
+    public int Number { get; set; }
+    public string Date { get; set; }
+    public string Phone { get; set; }
+    public string CityFrom { get; set; }
+    public string CityTo { get; set; }
+
+    public void ReadFromConsole()
+    {
+        Console.Write("Enter number: ");
+        Number = int.Parse(Console.ReadLine());
+        Console.Write("Enter city to: ");
+        CityTo = Console.ReadLine();
+        Console.Write("Enter city from: ");
+        CityFrom = Console.ReadLine();
+        Console.Write("Enter phone: ");
+        Phone = Console.ReadLine();
+        Console.Write("Enter date: ");
+        Date = Console.ReadLine();
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine($"Number: {Number}");
+        Console.WriteLine($"Date: {Date}");
+        Console.WriteLine($"Phone: {Phone}");
+        Console.WriteLine($"City from: {CityFrom}");
+        Console.WriteLine($"City to: {CityTo}");
+    }
+}
---
+++ exam nova poshta/Models/Service.cs
@@ -0,0 +1,32 @@
+﻿//Нова пошта
+//	Вивід послуг пошти
+//	Доповнення(редагування, видалення ) послуги
+//  Прийом замовлення про пересилку товару(друк квитанції, прийом товару, оплата, реєстрація про замовлення у базі) 
+//	Відмітка про виконання замовлення(доставку товарів)
+//	Зберігати та завантажувати у(з) файлі(у) замовлені послуги
+//	Виконані послуги зберігати у іншому файлі
+
+
+public class Service
+{
+    public double Price { get; set; }
+    public int Duration { get; set; }
+    public string Name { get; set; }
+    
+    public void ReadFromConsole()
+    {
+        Console.Write("Enter name: ");
+        Name = Console.ReadLine();
+        Console.Write("Enter duration: ");
+        Duration = int.Parse(Console.ReadLine());
+        Console.Write("Enter price: ");
+        Price = double.Parse(Console.ReadLine());
+    }
+    public void Show()
+    {
+        Console.WriteLine("-------------------------------------------");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Duration: {Duration}");
+        Console.WriteLine($"Price: {Price}");
+    }
+}
