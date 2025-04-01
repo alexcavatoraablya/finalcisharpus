fa100efb9eb4b3d54a7ed9719113b97be5242abf
alexcavatoraablya <ohrimchuksofi@gmail.com>
2025-04-01 14:38:15 +0300

    ytdj6r

--- exam nova poshta/Program.cs
+++ exam nova poshta/Program.cs
@@ -7,23 +7,13 @@
 //	Виконані послуги зберігати у іншому файлі
 
 
+using exam_nova_poshta;
 using System.Diagnostics;
 using System.Numerics;
 using System.Text.Json;
 using System.Xml.Linq;
 
-List<Package> items = new()
-{
-    new Package() { Number = 056, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Rivne"},
-    new Package() { Number = 057, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Zhitomur"},
-    new Package() { Number = 058, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Harkiv"},
-};
-
-List<Service> services = new()
-{
-    new Service() { Name = "Sending", Duration = 2, Price = 140 },
-    new Service() { Name = "Global Forvarding", Duration = 10, Price = 290 },
-};
+nova_poshta exam_nova_poshta = new();
 
 while (true)
 {
@@ -52,59 +42,38 @@ while (true)
             return 0;
 
         case 1:
-            Package newItem = new();
-            newItem.ReadFromConsole();
-            items.Add(newItem);
+            
             break;
 
         case 2:
-            Service newService = new();
-            newService.ReadFromConsole();
-            services.Add(newService);
+           
             break;
 
         case 3:
-            foreach (Package item in items)
-                item.Show();
+
             break;
 
         case 4:
-            foreach (Service service in services)
-                service.Show();
+
             break;
 
         case 5:
-            var jsonData = File.ReadAllText("database.json");
-            services = JsonSerializer.Deserialize<List<Service>>(jsonData);
+           
             break;
 
         case 6:
-            var json = JsonSerializer.Serialize(items);
-            File.WriteAllText("database.json", json);
-
-            var json2 = JsonSerializer.Serialize(services);
-            File.WriteAllText("services.json", json2);
+            
             break;
 
         case 7:
-            Console.WriteLine("Enter service to find: ");
-            string name = Console.ReadLine();
+
             break;
 
         case 8:
-            Console.Write("Enter service to delete: ");
-            int name2 = int.Parse(Console.ReadLine());
 
-            var found2 = items.Find(x => x.Number == name2);
-            if (found2 == null)
-            {
-                Console.WriteLine("Product not found!");
-                break;
-            }
-
-            items.Remove(found2);
             break;
     }
 
+    Console.ReadKey();
     Console.ReadKey();
 }
--- exam nova poshta/nova poshta.cs
+++ exam nova poshta/nova poshta.cs
@@ -2,11 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace exam_nova_poshta
 {
     internal class nova_poshta
     {
+        List<Package> items = new()
+        {
+            new Package() { Number = 056, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Rivne"},
+            new Package() { Number = 057, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Zhitomur"},
+            new Package() { Number = 058, Date = "03 10", Phone = "0973609912", CityFrom = "Kuiv", CityTo = "Harkiv"},
+        };
+
+        List<Service> services = new()
+        {
+            new Service() { Name = "Sending", Duration = 2, Price = 140 },
+            new Service() { Name = "Global Forvarding", Duration = 10, Price = 290 },
+        };
+
+
+        public void AddPackage()
+        {
+            Package newItem = new();
+            newItem.ReadFromConsole();
+            items.Add(newItem);
+        }
+        public void AddService()
+        {
+            Service newService = new();
+            newService.ReadFromConsole();
+            services.Add(newService);
+        }
+        public void ShowPackage()
+        {
+            foreach (Package item in items)
+                item.Show();
+        }
+        public void ShowService()
+        {
+            foreach (Service service in services)
+                service.Show();
+        }
+        public void LoadService()
+        {
+            var jsonData = File.ReadAllText("database.json");
+            services = JsonSerializer.Deserialize<List<Service>>(jsonData);
+        }
+        public void SaveService()
+        {
+            var json = JsonSerializer.Serialize(items);
+            File.WriteAllText("database.json", json);
+
+            var json2 = JsonSerializer.Serialize(services);
+            File.WriteAllText("services.json", json2);
+        }
+        public void FindService()
+        {
+            Console.WriteLine("Enter service to find: ");
+            string name = Console.ReadLine();
+        }
+        public void DeleteService()
+        {
+            Console.Write("Enter service to delete: ");
+            int name2 = int.Parse(Console.ReadLine());
+
+            var found2 = items.Find(x => x.Number == name2);
+            if (found2 == null)
+            {
+                Console.WriteLine("Product not found!");
+                return;
+            }
+
+            items.Remove(found2);
+        }
     }
 }
